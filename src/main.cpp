84d5ee8d28596fb68b24ce78132733df3c248308
Christian Walter <cwalter@eab.com>
2020-04-20 22:01:25 -0400

    p,y,r for two cubes

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <bitset>
 #include <map>
 #include <vector>
-#include <algorithm>
+#include <math.h>
 #include <iostream>
 
 // SDL imports
@@ -20,15 +20,16 @@
 #define PI (3.14159)
 #define FPS (30)
 #define Z (12)  // depth from camera to render
-#define FOV (250)
-#define NEST_WIDTH (3)
-const int NEST_VOLUME = NEST_WIDTH * NEST_WIDTH * NEST_WIDTH;
+#define FOV (450)
+#define NEST_WIDTH (9)
+const int NW_2 = NEST_WIDTH * NEST_WIDTH;
+const int NW_3 = NEST_WIDTH * NEST_WIDTH * NEST_WIDTH;
 
 struct vertex  // rendering 3D graphics
 {
-  int x;
-  int y;
-  int z;
+  float x;
+  float y;
+  float z;
 };
 
 enum game_state
@@ -39,6 +40,13 @@ enum game_state
   menu,
 };
 
+struct quat
+{
+  float r;
+  float i;
+  float j;
+  float k;
+};
 
 void DrawWire(SDL_Renderer * renderer, float x1, float y1, float z1, float x2, float y2, float z2)
 {
@@ -57,16 +65,110 @@ void DrawWire(SDL_Renderer * renderer, vertex p1, vertex p2)
   DrawWire(renderer, p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
 }
 
-bool Collision(int target_ind, std::vector<int> nest[]) {
+quat QuatMul(quat p, quat q)
+{
+  // (ae-bf-cg-dh)+(af+be+ch-dg){i} +(ag-bh+ce+df){j} +(ah+bg-cf+de){k}
+  float r = p.r*q.r - p.i*q.i - p.j*q.j - p.k*q.k;
+  float i = p.r*q.i + p.i*q.r + p.j*q.k - p.k*q.j;
+  float j = p.r*q.j - p.i*q.k + p.j*q.r + p.k*q.i;
+  float k = p.r*q.k + p.i*q.j - p.j*q.i + p.k*q.r;
+  return {r, i, j, k};
+}
+
+quat ToQuaternion(float yaw, float pitch, float roll) // yaw (Z), pitch (Y), roll (X)
+{
+    // Abbreviations for the various angular functions
+    float cy = cos(yaw * 0.5);
+    float sy = sin(yaw * 0.5);
+    float cp = cos(pitch * 0.5);
+    float sp = sin(pitch * 0.5);
+    float cr = cos(roll * 0.5);
+    float sr = sin(roll * 0.5);
+
+    quat q;
+    q.r = cr * cp * cy + sr * sp * sy;
+    q.i = sr * cp * cy - cr * sp * sy;
+    q.j = cr * sp * cy + sr * cp * sy;
+    q.k = cr * cp * sy - sr * sp * cy;
+
+    return q;
+}
+
+void DrawCube(SDL_Renderer * renderer, float ox, float oy, float oz, float pitch, float yaw, float roll, int red, int grn, int blu)
+{
+  int center_x = WINDOW_WIDTH / 2;
+  int center_y = WINDOW_HEIGHT / 2;
+  int u, v;
+  float x,y,z, inv_mag_p, mag_p, p_diff;
+  quat p;
+  quat q = ToQuaternion(pitch, yaw, roll);
+  quat inv_q = {q.r, -q.i, -q.j, -q.k};
+  const float w = 1;
+  float space = w/NEST_WIDTH; // 1/nw * w
+  float half_width = w/2;
+  SDL_Point points[NW_3];
+  SDL_Point out_points[NW_3*4]; // fading points
+  for (int i=0; i<NW_3; ++i) {
+    x = (i % NEST_WIDTH) * space - half_width;
+    y = ((i / NEST_WIDTH)%NEST_WIDTH) * space - half_width;
+    z = (i / (NW_2)) * space - half_width;
+    p_diff = pow(x,2)+pow(y,2)+pow(z,2);
+    mag_p = sqrt(p_diff);
+    inv_mag_p = 1/mag_p;
+    p = {0, x*inv_mag_p, y*inv_mag_p, z*inv_mag_p};
+    // printf("%f %f %f\n", x, y, z);
+    p = QuatMul(QuatMul(q, p), inv_q); // pqp^-1
+    x=p.i*mag_p+ox;
+    y=p.j*mag_p+oy;
+    z=p.k*mag_p+oz;
+    // if (x < ox-half_width || x>ox+half_width ||
+    //   y < oy-half_width || y>oy+half_width ||
+    //   z < oz-half_width || z>oz+half_width) {
+
+    // }
+    if (z>0) {
+      u = FOV*x/z+center_x;
+      v = FOV*y/z+center_y;
+      points[i] = {u, v};
+      for (int j=0; j<4; ++j){
+        out_points[4*i+j] = {u+(j%2)?-1:1, u+(j/2)?-1:1};
+      }
+      points[i] = {int(FOV*x/z+center_x), int(FOV*y/z+center_y)};
+    } else {
+      points[i] = {-1, -1}; // off screen
+      for (int j=0; j<4; ++j){
+        out_points[4*i+j] = {-1, -1};
+      }
+      printf("%f %f %f\n\n", x, y, z);
+    }
+  }
+  SDL_SetRenderDrawColor(renderer, red, grn, blu, 255);
+  SDL_RenderDrawPoints(renderer, points, NW_3);
+
+  // TODO: experiment with alpha channel
+  SDL_SetRenderDrawColor(renderer, 4*red/5, 4*grn/5, 4*blu/5, 255);
+  SDL_RenderDrawPoints(renderer, points, NW_3*4);
+}
+
+bool Collision(int target_ind, std::vector<int> nest[])
+{
   // return nest[ind].size() > 0;
   return true;
 }
 
 game_state PlayLoop(SDL_Renderer* rend, TTF_Font* font,
-  std::vector<int> player_nest[], std::vector<int> target_nest[], int* current_ind) {
+  std::vector<int> player_nest[], std::vector<int> target_nest[], int* current_ind)
+{
     std::map<int,bool> keys;
-    const int INPUT_POLL_MAX = 10; // number of frames between repeated inputs
+    const int INPUT_POLL_MAX = 5; // number of frames between repeated inputs
     int input_poll_t = 0; // on key down, set to max, on key up, reset to 0
+
+    float a = 0.0;
+    float b = 1.0;
+    float c = 0.0;
+    float pitch = 0.0;
+    float yaw = 0.0;
+    float roll = 0.0;
 
     bool quit_state = false;
     bool pause_state = false;
@@ -87,21 +189,30 @@ game_state PlayLoop(SDL_Renderer* rend, TTF_Font* font,
         bool left = keys[SDLK_LEFT];
         bool right = keys[SDLK_RIGHT];
         bool down = keys[SDLK_DOWN];
-        bool w_key = keys[SDLK_w];
-        bool a_key = keys[SDLK_a];
-        bool s_key = keys[SDLK_s];
-        bool d_key = keys[SDLK_d];
-        bool dir_key_pressed = (w_key || a_key || s_key || d_key);
-        if (!dir_key_pressed) {
-          input_poll_t = 0;
-        } else if (input_poll_t) {
-          --input_poll_t;
-        } else {
-          // process input
+        bool up__key = keys[SDLK_w] || keys[SDLK_UP];
+        bool rgt_key = keys[SDLK_a] || keys[SDLK_RIGHT];
+        bool dwn_key = keys[SDLK_s] || keys[SDLK_DOWN];
+        bool lft_key = keys[SDLK_d] || keys[SDLK_LEFT];
+        bool dir_key_pressed = (up__key || rgt_key || dwn_key || lft_key);
+          // input_poll_t = INPUT_POLL_MAX;
+        if (up__key) {
+          roll -= 0.2;
         }
-
+        if (dwn_key) {
+          roll += 0.2;
+        }
+        if (lft_key) {
+          yaw += 0.2;
+        }
+        if (rgt_key) {
+          yaw -= 0.2;
+        }
         // clear the window
         SDL_RenderClear(rend);
+
+        DrawCube(rend, -1, 0, 3, pitch, yaw, roll, 0, 255, 0);
+        DrawCube(rend, 1, 0, 3, pitch, yaw, roll, 255, 0, 255);
+
         SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
         SDL_RenderPresent(rend);
 
@@ -114,7 +225,8 @@ game_state PlayLoop(SDL_Renderer* rend, TTF_Font* font,
 }
 
 game_state PauseLoop(SDL_Renderer* rend, TTF_Font* font,
-  std::vector<int> player_nest[], std::vector<int> target_nest[], int* current_ind) {
+  std::vector<int> player_nest[], std::vector<int> target_nest[], int* current_ind)
+{
     std::map<int,bool> keys;
 
     SDL_Surface* message = TTF_RenderText_Solid(font, "PAUSED", {255, 255, 255});
@@ -191,7 +303,8 @@ game_state PauseLoop(SDL_Renderer* rend, TTF_Font* font,
     return game_state::quit;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // attempt to initialize graphics and timer system
     if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
@@ -228,11 +341,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    std::vector<int> player_nest[NEST_VOLUME];
-    std::vector<int> target_nest[NEST_VOLUME];
+    std::vector<int> player_nest[NW_3];
+    std::vector<int> target_nest[NW_3];
     int current_ind = 0;
 
-    game_state state = game_state::pause;
+    game_state state = game_state::play;
     while(state!=game_state::quit) {
       switch(state) {
         case game_state::play:
